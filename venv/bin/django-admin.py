c23c36720c0b1141fb1d24cc2a1dd4cff3bcf842
SauL <sr.saul95@gmail.com>
2017-10-25 16:17:06 -0700

    mi commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/saul/WorkSpace/ProgramacionWeb/Unidad2/proyectos/examen_desarrolloweb1/venv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
